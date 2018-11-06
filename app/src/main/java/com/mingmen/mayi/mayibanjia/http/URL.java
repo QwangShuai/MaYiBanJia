5c061706a8143b4dfc1f8a675c677f60af43df5a
lwliuwei <76412840@qq.com>
2018-11-06 09:23:31 +0800

    11
    12

--- app/src/main/java/com/mingmen/mayi/mayibanjia/http/URL.java
+++ app/src/main/java/com/mingmen/mayi/mayibanjia/http/URL.java
@@ -9,7 +9,7 @@ public class URL {
 //    public static String url = "http://192.168.1.142:8080/ant/"; //毕森
 //    public static String url = "http://192.168.43.59:8080/ant/"; //毕森--
 //    public static String url = "http://192.168.1.125:8080/ant/"; //春波
-//    public static String url = "http://192.168.1.151:8080/ant/"; //刘哥
+//  public static String url = "http://192.168.1.152:8080/ant/"; //刘哥
 //    public static String url = "http://192.168.1.128:8080/ant/"; //李岩
-  public static String url = "http://www.canchengxiang.com/";//服务器
+   public static String url = "http://www.canchengxiang.com/";//服务器
 }

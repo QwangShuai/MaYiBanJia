7b0d82dab594532b0e0a33fe1fe3fa39ef4cbe66
lwliuwei <76412840@qq.com>
2018-11-26 09:11:06 +0800

    Revert "URL"
    This reverts commit 4fbc222305d907fc80f914d044e529a6a0e46186.

--- app/src/main/java/com/mingmen/mayi/mayibanjia/http/URL.java
+++ app/src/main/java/com/mingmen/mayi/mayibanjia/http/URL.java
@@ -7,9 +7,11 @@ package com.mingmen.mayi.mayibanjia.http;
 public class URL {
 //    public static String url = "http://192.168.1.155:8080/ant/"; //李岩
 //    public static String url = "http://192.168.1.155:8080/"; //李岩
-//    public static String url = "http://192.168.1.139:8080/ant/"; //毕森
+//    public static String url = "http://192.168.1.101:8080/ant/"; //毕森
+//    public static String url = "http://192.168.43.59:8080/ant/"; //毕森--
 //    public static String url = "http://192.168.1.125:8080/ant/"; //春波
 //    public static String url = "http://192.168.1.151:8080/ant/"; //刘哥
 //    public static String url = "http://192.168.1.128:8080/ant/"; //李岩
-      public static String url = "https://www.canchengxiang.com/";//服务器
+//  public static String url = "http://www.canchengxiang.com/";//服务器
+  public static String url = "https://www.canchengxiang.com/";//服务器
 }
